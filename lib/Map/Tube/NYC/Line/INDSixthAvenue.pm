b8d7321eb683668ec7789268dba4018319971b0b
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-07-30 10:37:56 +0100

    - Upgraded to use Map::Tube v3.00.

--- lib/Map/Tube/NYC/Line/INDSixthAvenue.pm
+++ lib/Map/Tube/NYC/Line/INDSixthAvenue.pm
@@ -1,6 +1,6 @@
 package Map::Tube::NYC::Line::INDSixthAvenue;
 
-$Map::Tube::NYC::Line::INDSixthAvenue::VERSION   = '0.23';
+$Map::Tube::NYC::Line::INDSixthAvenue::VERSION   = '0.24';
 $Map::Tube::NYC::Line::INDSixthAvenue::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::NYC::Line::INDSixthAvenue - NYC Subway Map: IND Sixth Avenue Line.
 
 =head1 VERSION
 
-Version 0.23
+Version 0.24
 
 =head1 DESCRIPTION
 
